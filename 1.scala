c9d4dce23a1a0b2f8823cc727ee20a5ce17ae401
HansPerz <62199429+HansPerz@users.noreply.github.com>
2021-08-20 21:10:49 +0530

    Update 1.scala

--- 1.scala
+++ 1.scala
@@ -1,1 +1,15 @@
-def 
+class Rational (x: Int, y:Int)  {
+  def numer = x
+  def denom = y
+  
+  def neg: Rational = new Rational(-numer, denom)
+  
+  def sub(r: Rational, s:Rational): Rational =
+    new Rational (r.numer * s.denom - s.numer * r.denom, r.denom * s.denom)
+  
+  def makeString(r: Rational) = 
+    r.numer + "/" + r.denom
+  
+  makeString(sub(new Rational(3,4), new Rational(5,8)))
+  
+  
